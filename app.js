3e576ae1d76648bf9029ba4b626a88dced254676
J. Cohen <hostelmarketing@gmail.com>
2014-10-12 14:16:35 -0700

    Versioned API example

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 var restify = require("restify");
 
-function respond(req, res, next) {
-    res.send("hello" + req.params.name);
-    next();
+var server = restify.createServer();
+
+function sendV1(req, res, next) {
+    res.send("Saluton: " + req.params.name);
+    return next();
 }
 
-var server = restify.createServer();
-server.get("/saluton/:nomo", respond);
-server.head("/saluton/:nomo", respond);
+function sendV2(req, res, next) {
+    res.send({ hello: req.params.name });
+    return next();
+}
+
+// Use Accept-Version
+// curl -s -H 'accept-version ~1' localhost:8080/saluton/Mondo
+// curl -s -H 'accept-version ~2' localhost:8080/saluton/Mondo
+// TODO: this isn't working
+var PATH = "/saluton/:name";
+
+server.get({
+    path: PATH,
+    version: "1.1.3"
+}, sendV1);
+
+server.get({
+    path: PATH,
+    version: "2.0.0"
+}, sendV2);
 
 server.listen(8080, function () {
     console.log("%s listening at %s", server.name, server.url);
